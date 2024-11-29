4bf806315d2d2d26071c337154a1785c3b14945c
Youenn Fablet <youenn@apple.com>
2024-11-29 02:10:20 -0800

    Cherry-pick 283286.534@safari-7620-branch (39b5b5e24156). rdar://140448767
        `SubscribeToCookieChangeNotifications` and `UnsubscribeFromCookieChangeNotifications` IPC messages do not verify cookie access
        rdar://140448767
    
        Reviewed by Chris Dumez.
    
        Add MESSAGE_CHECK to subscribeToCookieChangeNotifications and unsubscribeFromCookieChangeNotifications as they may come from WebProcess.
        These are similar checks to NetworkConnectionToWebProcess::domCookiesForHost and are necessary for subscribeToCookieChangeNotifications,
        as it would otherwise allow a WebProcess to learn about new cookies.
    
        * Source/WebKit/NetworkProcess/NetworkConnectionToWebProcess.cpp:
        (WebKit::NetworkConnectionToWebProcess::subscribeToCookieChangeNotifications):
        (WebKit::NetworkConnectionToWebProcess::unsubscribeFromCookieChangeNotifications):
    
        Canonical link: https://commits.webkit.org/283286.534@safari-7620-branch
    
    Canonical link: https://commits.webkit.org/282416.398@webkitglib/2.46

--- Source/WebKit/NetworkProcess/NetworkConnectionToWebProcess.cpp
+++ Source/WebKit/NetworkProcess/NetworkConnectionToWebProcess.cpp
@@ -951,6 +951,9 @@ void NetworkConnectionToWebProcess::domCookiesForHost(const URL& url, Completion
 
 void NetworkConnectionToWebProcess::subscribeToCookieChangeNotifications(const String& host)
 {
+    MESSAGE_CHECK(HashSet<String>::isValidValue(host));
+    MESSAGE_CHECK(protectedNetworkProcess()->allowsFirstPartyForCookies(m_webProcessIdentifier, URL { host }) != NetworkProcess::AllowCookieAccess::Terminate);
+
     ASSERT(!m_hostsWithCookieListeners.contains(host));
     m_hostsWithCookieListeners.add(host);
 
@@ -960,6 +963,9 @@ void NetworkConnectionToWebProcess::subscribeToCookieChangeNotifications(const S
 
 void NetworkConnectionToWebProcess::unsubscribeFromCookieChangeNotifications(const String& host)
 {
+    MESSAGE_CHECK(HashSet<String>::isValidValue(host));
+    MESSAGE_CHECK(protectedNetworkProcess()->allowsFirstPartyForCookies(m_webProcessIdentifier, URL { host }) != NetworkProcess::AllowCookieAccess::Terminate);
+
     bool removed = m_hostsWithCookieListeners.remove(host);
     ASSERT_UNUSED(removed, removed);
 
