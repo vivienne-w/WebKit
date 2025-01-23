33766a2ff194a355c8913360ced162d48547d7d6
Chris Dumez <cdumez@apple.com>
2025-01-23 07:34:39 -0800

    Cherry-pick 289288@main (93d2569cbae1). https://bugs.webkit.org/show_bug.cgi?id=286300
        Crash under WorkerWorkerAgent::connectToAllWorkerInspectorProxies() on the bots
        https://bugs.webkit.org/show_bug.cgi?id=286300
    
        Reviewed by Devin Rousso.
    
        WorkerWorkerAgent::connectToAllWorkerInspectorProxies() was getting called off the main thread
        and iterating over the WeakHashSet returned by `WorkerInspectorProxy::allWorkerInspectorProxiesCopy()`.
        The loop in connectToAllWorkerInspectorProxies() was ref'ing each WorkerInspectorProxy before checking
        if the proxy is relevant for the current agent. This ref'ing was needed for safety since the proxy could
        get destroyed concurrently on another thread otherwise. However, WorkerInspectorProxy is not
        ThreadSafeRefCounted and we were using WeakPtr (instead of ThreadSafeWeakPtr). As a result, the ref'ing
        of the object on another thread wasn't safe. Making WorkerInspectorProxy thread safe is not trivial since
        it holds strings.
    
        To make the code thread-safe, the global map of WorkerInspectorProxy now stores the context identifier
        as key. When looking up the context identifier in the HashMap, we're holding a lock so we're safe on this
        front. We then ref the proxies and add them to a Vector while holding the lock. The ref'ing is safe here
        before we know we're only ref'ing the proxies for the right context and thus on the correct thread.
    
        * Source/WebCore/inspector/agents/page/PageWorkerAgent.cpp:
        (WebCore::PageWorkerAgent::connectToAllWorkerInspectorProxies):
        * Source/WebCore/inspector/agents/worker/WorkerWorkerAgent.cpp:
        (WebCore::WorkerWorkerAgent::connectToAllWorkerInspectorProxies):
        * Source/WebCore/workers/WorkerInspectorProxy.cpp:
        (WebCore::pageOrWorkerGlobalScopeIdentifier):
        (WebCore::WTF_REQUIRES_LOCK):
        (WebCore::WorkerInspectorProxy::forEachProxyInContext):
        (WebCore::WorkerInspectorProxy::workerStarted):
        (WebCore::WorkerInspectorProxy::workerTerminated):
        (WebCore::WorkerInspectorProxy::allWorkerInspectorProxiesCopy): Deleted.
        * Source/WebCore/workers/WorkerInspectorProxy.h:
        * Source/WebCore/workers/WorkerMessagingProxy.cpp:
        (WebCore::WorkerMessagingProxy::startWorkerGlobalScope):
    
        Canonical link: https://commits.webkit.org/289288@main
    
    Canonical link: https://commits.webkit.org/282416.409@webkitglib/2.46

--- Source/WebCore/workers/WorkerInspectorProxy.h
+++ Source/WebCore/workers/WorkerInspectorProxy.h
@@ -25,14 +25,14 @@
 
 #pragma once
 
-#include <wtf/CheckedPtr.h>
-#include <wtf/CheckedRef.h>
-#include <wtf/FastMalloc.h>
+#include "PageIdentifier.h"
+#include "ScriptExecutionContextIdentifier.h"
+#include <variant>
+#include <wtf/Function.h>
 #include <wtf/RefCounted.h>
 #include <wtf/RefPtr.h>
 #include <wtf/TZoneMallocInlines.h>
 #include <wtf/URL.h>
-#include <wtf/WeakHashSet.h>
 #include <wtf/text/WTFString.h>
 
 // All of these methods should be called on the Main Thread.
@@ -70,7 +70,8 @@ public:
 
     // A Worker's inspector messages come in and go out through the Page's WorkerAgent.
 
-    static WeakHashSet<WorkerInspectorProxy> allWorkerInspectorProxiesCopy();
+    static Vector<Ref<WorkerInspectorProxy>> proxiesForPage(PageIdentifier);
+    static Vector<Ref<WorkerInspectorProxy>> proxiesForWorkerGlobalScope(ScriptExecutionContextIdentifier);
 
     const URL& url() const { return m_url; }
     const String& name() const { return m_name; }
@@ -78,7 +79,7 @@ public:
     ScriptExecutionContext* scriptExecutionContext() const { return m_scriptExecutionContext.get(); }
 
     WorkerThreadStartMode workerStartMode(ScriptExecutionContext&);
-    void workerStarted(ScriptExecutionContext*, WorkerThread*, const URL&, const String& name);
+    void workerStarted(ScriptExecutionContext&, WorkerThread*, const URL&, const String& name);
     void workerTerminated();
 
     void resumeWorkerIfPaused();
@@ -90,7 +91,14 @@ public:
 private:
     explicit WorkerInspectorProxy(const String& identifier);
 
+    using PageOrWorkerGlobalScopeIdentifier = std::variant<PageIdentifier, ScriptExecutionContextIdentifier>;
+    static std::optional<PageOrWorkerGlobalScopeIdentifier> pageOrWorkerGlobalScopeIdentifier(ScriptExecutionContext&);
+
+    void addToProxyMap();
+    void removeFromProxyMap();
+
     RefPtr<ScriptExecutionContext> m_scriptExecutionContext;
+    std::optional<PageOrWorkerGlobalScopeIdentifier> m_contextIdentifier;
     RefPtr<WorkerThread> m_workerThread;
     String m_identifier;
     URL m_url;
