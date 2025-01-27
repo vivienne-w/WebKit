4dac59e15f9c1ed1607670c962a0a6ea7760c0ff
Philippe Normand <philn@igalia.com>
2025-01-27 04:50:23 -0800

    Cherry-pick 289420@main (0f278769c3b9). https://bugs.webkit.org/show_bug.cgi?id=285813
        REGRESSION(287950@main): [GStreamer] http/wpt/mediarecorder/pause-recording.html consistently times out
        https://bugs.webkit.org/show_bug.cgi?id=285813
    
        Reviewed by Xabier Rodriguez-Calvar.
    
        The test was timing out because it was expecting an EOS event to reach the sink of an already EOS'd
        pipeline. To prevent this from happening the recorder no longer does the wait if the EOS event
        wasn't successfully pushed in the pipeline.
    
        * LayoutTests/platform/glib/TestExpectations:
        * Source/WebCore/platform/mediarecorder/MediaRecorderPrivateGStreamer.cpp:
        (WebCore::MediaRecorderPrivateBackend::stopRecording):
        * Source/WebCore/platform/mediastream/gstreamer/GStreamerMediaStreamSource.cpp:
        (webkitMediaStreamSrcSignalEndOfStream):
        * Source/WebCore/platform/mediastream/gstreamer/GStreamerMediaStreamSource.h:
    
        Canonical link: https://commits.webkit.org/289420@main
    
    Canonical link: https://commits.webkit.org/282416.435@2.46-jan-2025-multimedia

--- Source/WebCore/platform/mediarecorder/MediaRecorderPrivateGStreamer.cpp
+++ Source/WebCore/platform/mediarecorder/MediaRecorderPrivateGStreamer.cpp
@@ -173,9 +173,9 @@ void MediaRecorderPrivateBackend::stopRecording(CompletionHandler<void()>&& comp
     GST_DEBUG_OBJECT(m_transcoder.get(), "Stop requested, pushing EOS event");
 
     auto scopeExit = makeScopeExit([this, completionHandler = WTFMove(completionHandler)]() mutable {
+        GST_DEBUG_OBJECT(m_transcoder.get(), "Tearing down pipeline");
         unregisterPipeline(m_pipeline);
         m_pipeline.clear();
-        GST_DEBUG_OBJECT(m_transcoder.get(), "Stopping");
         m_transcoder.clear();
         completionHandler();
     });
@@ -185,7 +185,13 @@ void MediaRecorderPrivateBackend::stopRecording(CompletionHandler<void()>&& comp
         m_eos = true;
         return;
     }
-    webkitMediaStreamSrcSignalEndOfStream(WEBKIT_MEDIA_STREAM_SRC(m_src.get()));
+
+    GST_DEBUG_OBJECT(m_transcoder.get(), "Emitting EOS event(s)");
+    if (!webkitMediaStreamSrcSignalEndOfStream(WEBKIT_MEDIA_STREAM_SRC(m_src.get()))) {
+        GST_DEBUG_OBJECT(m_transcoder.get(), "EOS event(s) un-successfully sent, not expecting them on the sink");
+        m_eos = true;
+        return;
+    }
 
     bool isEOS = false;
     while (!isEOS) {
@@ -197,6 +203,7 @@ void MediaRecorderPrivateBackend::stopRecording(CompletionHandler<void()>&& comp
         });
         isEOS = m_eos;
     }
+    GST_DEBUG_OBJECT(m_transcoder.get(), "EOS event received on sink");
 }
 
 void MediaRecorderPrivateBackend::fetchData(MediaRecorderPrivate::FetchDataCallback&& completionHandler)
